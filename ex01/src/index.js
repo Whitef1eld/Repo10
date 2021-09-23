21e46988ae84b3093ca28c6593b0a865f729460c
Whitef1eld <nwhitefiel1@etf.unsa.ba>
2021-09-23 09:29:43 +0200

    Objects exercise

---
+++ ex01/src/index.js
@@ -0,0 +1,14 @@
+// only change code below this line
+var myPet = {
+    species: "cat",
+    name: "Bobi",
+    legs: 4,
+    friends: ["gugi", "mugi"]
+};
+
+function myFunction(myObj) {
+    return myObj;
+}
+// only change code above this line
+console.log(myFunction(myPet)); // change this line
+module.exports = { myPet, myFunction };
